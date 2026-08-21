1c9796a470038b86641994a158084cad4824b2c0
Sutanuka Ganguly <gangulysutanuka2002@gmail.com>
2026-08-21 19:53:54 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int rows,cols;
+    private Set<Pair<Integer,Integer>>path = new HashSet<>();
+
+    public boolean exist(char[][] board, String word) {
+        rows=board.length;
+        cols=board[0].length;
+        for(int r=0; r<rows; r++){
+            for(int c=0;c<cols;c++){
+                if(dfs(board,word,r,c,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean dfs(char[][]board, String word, int r, int c, int i){
+        if(i==word.length()){
+            return true;
+        }
+        if(r<0 || c<0 || r>=rows || c>=cols || board[r][c]!=word.charAt(i) || path.contains(new Pair<>(r,c))){
+            return false;
+        }
+        path.add(new Pair<>(r,c));
+        boolean res = dfs(board,word,r+1,c,i+1) ||
+                      dfs(board,word,r-1,c,i+1) ||
+                      dfs(board,word,r,c+1,i+1) ||
+                      dfs(board,word,r,c-1,i+1);
+        path.remove(new Pair<>(r,c));
+        return res;
+    }
+}
